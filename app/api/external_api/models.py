d674c8c07e286c9e7915bb3c7578224290a70094
Vinit Shah <shahvinit2244@gmail.com>
2020-03-01 10:22:43 -0500

    nc data

--- app/api/external_api/models.py
+++ app/api/external_api/models.py
@@ -12,12 +12,39 @@ def fetchDataFromStLouisFed(api_key, api_url, series_id):
     }
     response = requests.get(url=api_url, params=metadata)
     if response.status_code == 200:
-        json_object = json.loads(response.text)
+        return json.loads(response.text)
+    else:
+        return None
+
+
+def filterByState(json_object, state):
+    if json_object and state:
         data_list = json_object['meta']['data']['2018']
         json_data = []
         for index in range(len(data_list)):
-            if 'NC' in data_list[index]['region']:
+            if state in data_list[index]['region']:
                 json_data.append(data_list[index])
         return json_data
     else:
         return None
+
+
+def getJsonDataByNC():
+    api_key = 'adeeb2c76d8bb84bbc115f84d23399d4'
+    series_id = 'S1701ACS037179'
+    api_url = 'https://api.stlouisfed.org/geofred/series/data'
+    state = 'NC'
+    json_output = fetchDataFromStLouisFed(api_key, api_url, series_id)
+    print(json_output)
+    nc_data = filterByState(json_output, state)
+    print(nc_data)
+    dict = {}
+    dict['title'] = json_output['meta']['title']
+    dict['units'] = json_output['meta']['units']
+    dict['frequency'] = json_output['meta']['frequency']
+    dict[state] = nc_data
+    with open('nc_data_test.json', 'w', encoding='utf-8') as json_file:
+        json.dump(dict, json_file, ensure_ascii=False, indent=2)
+
+
+getJsonDataByNC()
--- app/api/external_api/routes.py
+++ app/api/external_api/routes.py
@@ -11,15 +11,10 @@ api = Api(external_api_blueprint)
 
 class APIList(Resource):
     def get(self):
-        api_key = os.getenv('GEOFRED_API_KEY')
-        series_id = 'S1701ACS037179'
-        api_url = 'https://api.stlouisfed.org/geofred/series/data'
-        json_output = external_api.fetchDataFromStLouisFed(
-            api_key, api_url, series_id)
-        if json_output:
-            return make_response(jsonify({'poverty data': json_output}), 200)
+        json_data = external_api.getJsonDataByNC()
+        if json_data:
+            return make_response(jsonify(json_data, 200)
         else:
             return make_response("Internal Error occured", 404)
 
-
-api.add_resource(APIList, '/poverty')
+api.add_resource(APIList, '/poverty/NC')
