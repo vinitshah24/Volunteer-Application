f8736f09cb0788e4706c6270d8719c3ea11f60b5
jchickie <jchickie@uncc.edu>
2020-04-29 01:29:52 -0400

    update Charities page to use backend API and improve functionality and design

--- views/src/views/Charities.vue
+++ views/src/views/Charities.vue
@@ -1,31 +1,48 @@
 <template>
   <div class="charities">
-    <b-container class="mh-50">
-      <b-row>
-        <b-col v-for="charity in charities" :key="charity.ein">
+    <h1 class="display-4" style="margin-left:auto">Donate to these worthy charities in your area</h1>
+    <b-container v-for="rows in rowCount" :key="rows.id">
+      <b-card-group deck>
+        <b-col group deck v-for="column in numberOfColumns" :key="column.id">
           <b-card
-            :rating="charity.currentRating.rating"
-            :title="charity.charityName"
-            :sub-title="subTitle + charity.currentRating.rating"
+            v-if="charities.length >= cardCount(rows, column)"
+            :rating="cardCount(rows, column)"
+            :title="charities[cardCount(rows, column) - 1].charityName"
+            :sub-title="subTitle + charities[cardCount(rows, column) - 1].rating"
             img-src="https://picsum.photos/600/300/?image=25"
             img-alt="Image"
             img-top
             tag="article"
-            style="max-width: 20rem;"
+            style="min-height: 25rem; margin: 25px 0px"
             class="mb-2"
           >
-            <b-card-text>{{charity.mission.toString()}}</b-card-text>
+            <b-card-text>{{ charities[cardCount(rows, column) - 1].cause }}</b-card-text>
 
-            <b-button :href="charity.charityNavigatorURL" variant="primary">Donate!</b-button>
+            <template v-slot:footer>
+              <b-button :href="charities[column].charityNavigatorURL" variant="primary">Donate!</b-button>
+            </template>
           </b-card>
         </b-col>
-      </b-row>
-      <b-row></b-row>
+      </b-card-group>
     </b-container>
   </div>
 </template>
 
 <script>
+// var request = require("request");
+// var reqy = {
+//   method: "GET",
+//   url: "http://127.0.0.1:5000/api/v1/user\n",
+//   headers: {
+//     Authorization:
+//       "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODQ3NDY0MDIsIm5iZiI6MTU4NDc0NjQwMiwianRpIjoiYmU2Yjk0ZWEtNWIxYS00OWM0LTliYjgtNGVkOGQ4MzM2NDBjIiwiZXhwIjoxNTg0NzQ3MzAyLCJpZGVudGl0eSI6IjBjZjBjNmE3LTBmYWMtNGE4ZS1hMGJhLWY1YzFiM2M1YjU0ZCIsImZyZXNoIjp0cnVlLCJ0eXBlIjoiYWNjZXNzIn0.oGBCfh8RFNbdS4Q_CSHtQ6Rk1hO5uLqB-YD9LKY2944"
+//   }
+// };
+// request(options, function(error, response) {
+//   if (error) throw new Error(error);
+//   console.log(response.body);
+// });
+
 export default {
   name: "Charities",
   data() {
@@ -33,24 +50,51 @@ export default {
       charities: [],
       title: "",
       subTitle: "Current rating: ",
-      rating: 4
+      rating: 4,
+      numberOfColumns: 3
     };
   },
-  methods: {
-    fetchCharities: function() {
-      const baseURI = `https://api.data.charitynavigator.org/v2/Organizations?app_id=0de7ab2c&app_key=${process.env.VUE_APP_CHARITY}&pageSize=4&rated=true&state=NC`;
-      this.$http.get(baseURI).then(result => {
-        this.charities = result.data;
-        console.log(this.charities);
-      });
+  computed: {
+    rowCount: function() {
+      return Math.floor((this.charities.length - 1) / this.numberOfColumns) + 1;
     }
   },
-  async mounted() {
+  methods: {
+    cardCount: function(rows, columns) {
+      return (rows - 1) * this.numberOfColumns + columns;
+    },
+    fetchCharities: function() {
+      const baseURI = `http://127.0.0.1:5000/api/v1/charities/NC`;
+      this.$http
+        .get(baseURI, {
+          method: "GET",
+
+          headers: {
+            Authorization:
+              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1ODgxMzM2NDksIm5iZiI6MTU4ODEzMzY0OSwianRpIjoiNDNmODVkOWYtOGUzOC00OGEyLTgwNDMtNjdiY2YyZGI2Y2IzIiwiZXhwIjoxNTg4MTM0NTQ5LCJpZGVudGl0eSI6ImJlY2RiYmFmLTE0MGUtNDUyNC04NjM5LWExMjZlYjI0ZTk5ZSIsImZyZXNoIjp0cnVlLCJ0eXBlIjoiYWNjZXNzIn0.7FJi_ljS9S-CqSK2nDu-WLqY62zoUV8z9yVM07z6Vcw"
+          }
+        })
+        .then(result => {
+          console.log("request made");
+          console.log(result);
+          this.charities = result.data.charities;
+          console.log(this.charities);
+        });
+    }
+  },
+  async beforeMount() {
     try {
       await this.fetchCharities();
     } catch (error) {
       console.log(error);
     }
+  },
+  async mounted() {
+    // try {
+    //   await this.fetchCharities();
+    // } catch (error) {
+    //   console.log(error);
+    // }
   }
 };
 </script>
