ff0849a06a88be71c51880d8b7e775132691eb45
jchickie <jchickie@uncc.edu>
2020-04-29 22:51:12 -0400

    adjust sign in form size

--- views/src/views/SignIn.vue
+++ views/src/views/SignIn.vue
@@ -1,26 +1,40 @@
 <template>
   <div>
-    <b-container w-20>
-      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-        <b-form-group id="input-group-1" label="Username:" label-for="input-1">
-          <b-form-input
-            id="input-1"
-            v-model="form.username"
-            type="text"
-            required
-            placeholder="Enter username"
-          ></b-form-input>
-        </b-form-group>
+    <b-container mw-25>
+      <b-row>
+        <b-col></b-col>
+        <b-col>
+          <b-card>
+            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+              <b-form-group id="input-group-1" label="Username:" label-for="input-1">
+                <b-form-input
+                  id="input-1"
+                  v-model="form.username"
+                  type="text"
+                  required
+                  placeholder="Enter username"
+                ></b-form-input>
+              </b-form-group>
 
-        <b-form-group id="input-group-2" label="Password:" label-for="input-2">
-          <b-form-input id="input-2" v-model="form.password" required type="password" placeholder="Enter password"></b-form-input>
-        </b-form-group>
+              <b-form-group id="input-group-2" label="Password:" label-for="input-2">
+                <b-form-input
+                  id="input-2"
+                  v-model="form.password"
+                  required
+                  type="password"
+                  placeholder="Enter password"
+                ></b-form-input>
+              </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
-        <b-button type="reset" variant="danger">Reset</b-button>
-        <br />
-        <b-button to="/signup">Sign up</b-button>
-      </b-form>
+              <b-button type="submit" variant="primary">Submit</b-button>
+              <b-button type="reset" variant="danger">Reset</b-button>
+              <br />
+              <b-button to="/signup">Sign up</b-button>
+            </b-form>
+          </b-card>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
@@ -49,10 +63,13 @@ export default {
     onReset(evt) {
       evt.preventDefault();
       // Reset our form values
-      this.form.email = "";
+      this.form.username = "";
       this.form.password = "";
       // Trick to reset/clear native browser form validation state
       this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
     }
   }
 };
