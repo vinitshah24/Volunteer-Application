00cb8072f999fdc1625dc4e14131a6b8306ebdf7
jchickie <jchickie@uncc.edu>
2020-05-01 05:29:38 -0400

    edit create event form - add time and date pickers, select inputs, textarea

--- views/src/views/CreateEvent.vue
+++ views/src/views/CreateEvent.vue
@@ -3,75 +3,146 @@
     <b-container mw-25>
       <b-card header="Create an Event" header-tag="header" border-variant="primary">
         <b-form @submit="onSubmit" @reset="onReset" v-if="40">
-          <b-form-group id="input-group-1" label="Event Name:" label-for="input-1">
-            <b-form-input
-              id="input-1"
-              v-model="form.name"
-              type="text"
-              required
-              placeholder="The name of your event"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group id="input-group-2" label="Event Category" label-for="input-2">
-            <b-form-input
-              id="input-2"
-              v-model="form.category"
-              type="text"
-              required
-              placeholder="Enter the category of your event"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group id="input-group-3" label="Event Details" label-for="input-3">
-            <b-form-input
-              id="input-3"
-              v-model="form.details"
-              type="text"
-              required
-              placeholder="Enter the description of your event"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group
-            id="input-group-4"
-            label="Event Address"
-            label-for="input-4, input-5, input-6"
-          >
-            <b-form-input
-              id="input-3"
-              v-model="form.address"
-              type="text"
-              required
-              placeholder="Enter the street address of your event"
-            ></b-form-input>
-            <b-form-input
-              id="input-4"
-              v-model="form.county"
-              type="text"
-              required
-              placeholder="Enter the county of your event"
-            ></b-form-input>
-            <b-form-input
-              id="input-5"
-              v-model="form.state"
-              type="text"
-              required
-              placeholder="Enter the state of your event"
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group id="input-group-4" label="Date" label-for="input-6">
-            <b-form-input id="input-6" v-model="form.date" type="text" required placeholder="Date"></b-form-input>
-          </b-form-group>
-          <b-form-group id="input-group-5" label="Time" label-for="input-7">
-            <!-- <b-form-input
-              id="input-7"
-              v-model="form.time"
-              type="text"
-              required
-              placeholder="Enter Time"
-            ></b-form-input>-->
-            <b-form-timepicker v-model="timeValue" locale="en"></b-form-timepicker>
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="3"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              id="input-group-1"
+              label="Event Name:"
+              label-for="input-1"
+              label-align="left"
+            >
+              <b-form-input
+                id="input-1"
+                v-model="form.name"
+                type="text"
+                required
+                placeholder="The name of your event"
+              ></b-form-input>
+            </b-form-group>
+          </b-card>
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="3"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              id="input-group-2"
+              label="Event Category"
+              label-for="input-2"
+              label-align="left"
+            >
+              <b-form-select v-model="form.category" :options="categories">
+                <template v-slot:first>
+                  <b-form-select-option value disabled>-- Please select an option --</b-form-select-option>
+                </template>
+              </b-form-select>
+            </b-form-group>
+          </b-card>
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="3"
+              label="Event Details"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              label-align="left"
+              id="input-group-3"
+              label-for="form-textarea"
+            >
+              <b-form-textarea
+                id="form-textarea"
+                v-model="form.details"
+                placeholder="Enter a description of your event..."
+                rows="3"
+                max-rows="6"
+              ></b-form-textarea>
+            </b-form-group>
+          </b-card>
 
-            <div class="mt-2">Value: '{{ timeValue }}'</div>
-          </b-form-group>
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="2"
+              label="Event Address"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              label-align="left"
+            >
+              <b-form-group
+                label-cols-sm="1"
+                label="Street:"
+                label-align-sm="right"
+                label-for="nested-street"
+              >
+                <b-form-input v-model="street" id="nested-street"></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label-cols-sm="1"
+                label="City:"
+                label-align-sm="right"
+                label-for="nested-city"
+              >
+                <b-form-input v-model="city" id="nested-city"></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label-cols-sm="1"
+                label="State:"
+                label-align-sm="right"
+                label-for="nested-state"
+              >
+                <b-form-input v-model="form.state" id="nested-state"></b-form-input>
+              </b-form-group>
+
+              <b-form-group
+                label-cols-sm="1"
+                label="County:"
+                label-align-sm="right"
+                label-for="nested-county"
+              >
+                <b-form-input v-model="form.county" id="nested-county"></b-form-input>
+              </b-form-group>
+            </b-form-group>
+          </b-card>
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="3"
+              label="Date"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              id="input-group-4"
+              label-for="datepicker"
+              label-align="left"
+            >
+              <b-form-datepicker id="datepicker" v-model="form.date" class="mb-2"></b-form-datepicker>
+            </b-form-group>
+          </b-card>
+
+          <b-card bg-variant="light" class="mb-3">
+            <b-form-group
+              label-cols-lg="3"
+              label="Time"
+              label-size="lg"
+              label-class="font-weight-bold pt-0"
+              class="mb-0"
+              id="input-group-5"
+              label-for="timepicker"
+              label-align="left"
+            >
+              <b-form-timepicker
+                id="timepicker"
+                v-model="form.time"
+                placeholder="Choose a time"
+                reset-button
+                locale="en"
+              ></b-form-timepicker>
+            </b-form-group>
+          </b-card>
           <div></div>
 
           <b-button class="top-button" type="submit" variant="primary">Create Event</b-button>
@@ -96,9 +167,20 @@ export default {
         date: "",
         time: ""
       },
-
+      street: "",
+      city: "",
       show: true,
-      timeValue: ""
+      categories: [
+        { value: "Volunteering", text: "Volunteering" },
+        { value: "Education", text: "Education" },
+        { value: "Youth Outreach", text: "Youth Outreach" },
+        { value: "Food donations", text: "Food donations" },
+        { value: "Meal prep", text: "Meal prep" },
+        { value: "Shelter generation", text: "Shelter generation" },
+        { value: "Awareness building", text: "Awareness building" },
+        { value: "Political action", text: "Political action" },
+        { value: "Performing Arts", text: "Performing Arts" }
+      ]
     };
   },
   methods: {
@@ -106,6 +188,7 @@ export default {
       evt.preventDefault();
       // alert(JSON.stringify(this.form));
       console.log(this.$root.$router.app.$children[0]);
+      this.form.address = `${this.street}, ${this.city}`;
       this.createEvent(this.form);
     },
     onReset(evt) {
@@ -144,8 +227,8 @@ export default {
             this.$router.push("/events");
           })
           .catch(error => {
-            console.log(error.config);
-            alert(`Error: ${error.config.message}`);
+            console.log(error.response);
+            alert(`Error: ${error}`);
           });
       } else {
         alert("You must log in to create an event!");
